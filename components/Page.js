7bed7188bb08ba53c5758682ef8d914d6feeda8f
Ernesto A. Nappi <eanappi@gmail.com>
2019-05-17 13:09:59 -0300

    Add serialize to request

--- components/Page.js
+++ components/Page.js
@@ -2,17 +2,26 @@ import Header from './Header.js'
 import Footer from './Footer.js'
 import MdConvert from './MdConvert.js'
 
+const Data = {
+  mdText: ''
+}
+
 export default {
-  data: {
-    MdText: null
-  },
   oninit() {
+    m.request({
+      method: 'GET',
+      url: 'content/home.md',
+      deserialize: (mdTextRaw) => { return mdTextRaw }
+    })
+    .then((mdTextGet) => {
+      Data.mdText = mdTextGet
+    })
   },
   view(vnode) {
     return m('main',
       m(Header),
       m(MdConvert, {
-        text: '# LOLOLOL'
+        text: Data.mdText
       }),
       m(Footer),
     )
