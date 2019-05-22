9df9177f9e74ae571b02bdf24ffcc61df4464b30
Ernesto A. Nappi <eanappi@gmail.com>
2019-05-22 13:43:29 -0300

    Delete MdConvert.js

--- components/MdConvert.js
+++
@@ -1,9 +0,0 @@
-const MdConverter = new showdown.Converter()
-
-export default {
-  view(vnode) {
-    return m('.wrapper-md', 
-      m.trust(MdConverter.makeHtml(vnode.attrs.text ? vnode.attrs.text : ''))
-    )
-  }
-}
